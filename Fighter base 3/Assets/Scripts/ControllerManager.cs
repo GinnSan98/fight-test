d3d1c387486324d7834ab373b37f599b59307e81
SHyperB <35599114+SHyperB@users.noreply.github.com>
2018-07-30 16:34:43 +0100

    E V E R Y T H I N G
    Includes splashscreen code
    Includes Controller recognition
    Includes warning screen for no controller (unused)

---
+++ Fighter base 3/Assets/Scripts/ControllerManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Made by Connor-chan
+
+public class ControllerManager : MonoBehaviour
+{
+    public Texture2D controllerNotDetected;
+
+    public bool pS4Controller;                   //I'm setting up bools for different controllers so we can change some UI elements depending on whats plugged in
+    public bool xBOXController;
+    public bool controllerDetected;
+
+    public static bool startUpFinished;        //Might be confusing. Its just a bool for when the start up is finished
+
+    void Awake()
+    {
+        pS4Controller = false;
+        xBOXController = false;
+        controllerDetected = false;
+
+        startUpFinished = false;
+    }
+
+	void Start ()
+    {
+        DontDestroyOnLoad(this);        //The controller manager game object MUST remain active regardless of what scene is loaded
+
+	}
+
+	void Update ()
+    {
+	}
+
+    void LateUpdate()
+    {
+        string[] joyStickNames = Input.GetJoystickNames();
+
+        for (int js = 0; js < joyStickNames.Length; js++)
+        {
+            if (joyStickNames[js].Length == 19)
+            {
+                pS4Controller = true;
+                controllerDetected = true;
+
+            }
+            if (joyStickNames[js].Length == 33)
+            {
+                xBOXController = true;
+                controllerDetected = true;
+
+            }
+
+            if (joyStickNames[js].Length != 0)
+                return;
+
+            if (string.IsNullOrEmpty(joyStickNames[js]))
+                controllerDetected = false;
+
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (startUpFinished == false)
+            return;
+
+        if (controllerDetected == true)
+            return;
+
+        if (controllerDetected == false)
+            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), controllerNotDetected);
+
+
+    }
+}
+
---
+++ Fighter base 3/Assets/Scripts/Splashscreen.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Started by Connor on 26/07/2018
+
+//Tells script to attach audio source
+[RequireComponent(typeof(AudioSource))]
+
+public class Splashscreen : MonoBehaviour
+{
+    public Texture2D splashScreenBackground;
+    public Texture2D splashScreenText;
+
+    private AudioSource splashScreenAudio;
+    public AudioClip splashScreenMusic;
+
+    private float splashScreenFadeValue;
+    private float splashScreenFadeSpeed = 0.20f;
+
+    private SplashScreenController splashScreenController;
+
+    private enum SplashScreenController
+    {
+        SplashScreenFadeIn = 0,
+        SplashScreenFadeOut = 1
+
+    }
+
+    void Awake()
+    {
+        splashScreenFadeValue = 0;
+
+    }
+
+	void Start ()
+    {
+        //Change later!!!!
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        splashScreenAudio = GetComponent < AudioSource >();
+
+        splashScreenAudio.volume = 0;
+        splashScreenAudio.clip = splashScreenMusic;
+        splashScreenAudio.loop = true;
+        splashScreenAudio.Play();
+
+        splashScreenController = Splashscreen.SplashScreenController.SplashScreenFadeIn;
+
+        StartCoroutine("SplashScreenManager");  //Starts the SplashScreenManager function
+	}
+	
+
+	void Update ()
+    {
+		
+
+	}
+
+    private IEnumerator SplashScreenManager()
+    {
+        while (true)
+        {
+            switch (splashScreenController)
+            {
+                case SplashScreenController.SplashScreenFadeIn:
+                    SplashScreenFadeIn();
+                    break;
+                case SplashScreenController.SplashScreenFadeOut:
+                    SplashScreenFadeOut();
+                    break;
+
+            }
+            yield return null;
+        }
+    }
+
+    private void SplashScreenFadeIn()
+    {
+        Debug.Log("SplashScreenFadeIn");
+
+        splashScreenAudio.volume += splashScreenFadeSpeed * Time.deltaTime;  //this changes volume as the text fades
+        splashScreenFadeValue += splashScreenFadeSpeed * Time.deltaTime;
+
+        if (splashScreenFadeValue > 1)
+            splashScreenFadeValue = 1;
+
+        if (splashScreenFadeValue == 1)
+            splashScreenController = Splashscreen.SplashScreenController.SplashScreenFadeOut;
+    }
+    private void SplashScreenFadeOut()
+    {
+        Debug.Log("SplashScreenFadeOut");
+
+        splashScreenAudio.volume -= splashScreenFadeSpeed * Time.deltaTime;  
+        splashScreenFadeValue -= splashScreenFadeSpeed * Time.deltaTime;
+
+
+        if (splashScreenFadeValue < 0)
+            splashScreenFadeValue = 0;
+
+        if (splashScreenFadeValue == 0)
+            SceneManager.LoadScene("ControllerStuff");
+    }
+
+    private void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), splashScreenBackground);
+
+        GUI.color = new Color(1, 1, 1, splashScreenFadeValue);
+
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), splashScreenText);
+    }
+}
